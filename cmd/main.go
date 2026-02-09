2efafc0288a3475ec4c1cd04d95eb0554b402e45
ddrmin <deepthidamruvan@gmail.com>
2026-02-09 17:02:07 +0530

    feat: establish database connection in main function with error handling

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,27 @@ import (
 	"log/slog"
 	"os"
 	"github.com/DDRMin/GO-Backend/internal/env"
+	"context"
+	"github.com/jackc/pgx/v5"
 )
 
 
 func main() {
+	ctx := context.Background()
+
 	config := config{
 		addr: ":8080",
 		db: dbConfig{
 			dbUrl: env.GetString("DB_URL", "postgres://user:password@localhost:5432/mydb?sslmode=disable"),
 		},
 	}
+
+	conn,err := pgx.Connect(ctx, config.db.dbUrl)
+	if err != nil {
+		slog.Error("Unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
 
 	api := API{
 		config: config,
