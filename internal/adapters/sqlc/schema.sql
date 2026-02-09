de5268dabc6a08c34085685fc868228a1cf7bbff
ddrmin <deepthidamruvan@gmail.com>
2026-02-09 16:25:18 +0530

    feat: update products table schema to include quantity and created_at fields with constraints

--- internal/adapters/sqlc/schema.sql
+++ internal/adapters/sqlc/schema.sql
@@ -1,5 +1,7 @@
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
   id   BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name text    NOT NULL,
-  price  DECIMAL(10, 2) NOT NULL
+  price  DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
+  quantity INT NOT NULL DEFAULT 0,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
