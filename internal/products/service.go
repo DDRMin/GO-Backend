fece80104e947dafefd3816e59cc6c746a1dd495
ddrmin <deepthidamruvan@gmail.com>
2026-02-12 14:07:10 +0530

    refactor: Update ListProducts method signature and improve service initialization

--- internal/products/service.go
+++ internal/products/service.go
@@ -1,19 +1,30 @@
 package products
 
-import "context"
+import (
+	"context"
+
+	repo "github.com/DDRMin/GO-Backend/internal/adapters/sqlc"
+)
 
 type Service interface {
-	ListProducts(ctx context.Context) (error)
+	ListProducts(ctx context.Context) error
 }
 
 type service struct {
-	
+	repo repo.Querier
 }
 
-func NewService() Service {
-	return &service{}
+func NewService(repo repo.Querier) Service {
+	return &service{repo: repo}
 }
 
-func (s *service) ListProducts(ctx context.Context) (error) {
+func (s *service) ListProducts(ctx context.Context) error {
+	products, err := s.repo.ListProducts(ctx)
+	if err != nil {
+		return err
+	}
+
+	_ = products // TODO: Use products in handler
+
 	return nil
 }
