c910fecb18f0fa1b7cff37589ac4e0c3daf76e94
ddrmin <deepthidamruvan@gmail.com>
2026-02-12 13:47:01 +0530

    feat: Add initial database schema for products and configure Atlas and SQLC for schema management and code generation.

--- internal/adapters/migrations/schema.sql
+++ internal/adapters/migrations/schema.sql
@@ -1,10 +1,14 @@
--- This file is used by sqlc to generate Go code.
--- Keep in sync with schema.hcl (the Atlas source of truth).
-
-CREATE TABLE IF NOT EXISTS products (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name text    NOT NULL,
-  price  DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
-  quantity INT NOT NULL DEFAULT 0,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- Add new schema named "public"
+CREATE SCHEMA IF NOT EXISTS "public";
+-- Set comment to schema: "public"
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+-- Create "products" table
+CREATE TABLE "public"."products" (
+  "id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+  "name" text NOT NULL,
+  "price" numeric(10,2) NOT NULL,
+  "quantity" integer NOT NULL DEFAULT 0,
+  "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "products_price_check" CHECK (price >= (0)::numeric)
 );
