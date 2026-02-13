7d74542a854628f179c9bab237b3d75bcb9b0f2c
ddrmin <deepthidamruvan@gmail.com>
2026-02-13 16:34:48 +0530

    feat: Add orders and order_items tables with corresponding models

--- internal/adapters/migrations/schema.hcl
+++ internal/adapters/migrations/schema.hcl
@@ -40,3 +40,72 @@ table "products" {
     expr = "price >= 0"
   }
 }
+
+table "orders" {
+  schema = schema.public
+
+  column "id" {
+    type = bigint
+    identity {
+      generated = ALWAYS
+    }
+  }
+
+  column "user_id" {
+    type = bigint
+    null = false
+  }
+
+  column "created_at" {
+    type    = timestamptz
+    null    = false
+    default = sql("CURRENT_TIMESTAMP")
+  }
+
+  primary_key {
+    columns = [column.id]
+  }
+}
+
+table "order_items" {
+  schema = schema.public
+
+  column "id" {
+    type = bigint
+    identity {
+      generated = ALWAYS
+    }
+  }
+
+  column "order_id" {
+    type = bigint
+    null = false
+  }
+
+  column "product_id" {
+    type = bigint
+    null = false
+  }
+
+  column "quantity" {
+    type = int
+    null = false
+    default = 1
+  }
+
+  primary_key {
+    columns = [column.id]
+  }
+
+  foreign_key "order_items_order_id_fkey" {
+    columns     = [column.order_id]
+    ref_columns = [table.orders.column.id]
+    on_delete   = "CASCADE"
+  }
+
+  foreign_key "order_items_product_id_fkey" {
+    columns     = [column.product_id]
+    ref_columns = [table.products.column.id]
+    on_delete   = "CASCADE"
+  }
+}
--- internal/adapters/migrations/schema.sql
+++ internal/adapters/migrations/schema.sql
@@ -2,6 +2,13 @@
 CREATE SCHEMA IF NOT EXISTS "public";
 -- Set comment to schema: "public"
 COMMENT ON SCHEMA "public" IS 'standard public schema';
+-- Create "orders" table
+CREATE TABLE "public"."orders" (
+  "id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+  "user_id" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("id")
+);
 -- Create "products" table
 CREATE TABLE "public"."products" (
   "id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -11,4 +18,14 @@ CREATE TABLE "public"."products" (
   "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY ("id"),
   CONSTRAINT "products_price_check" CHECK (price >= (0)::numeric)
+);
+-- Create "order_items" table
+CREATE TABLE "public"."order_items" (
+  "id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+  "order_id" bigint NOT NULL,
+  "product_id" bigint NOT NULL,
+  "quantity" integer NOT NULL DEFAULT 1,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "public"."orders" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
 );
